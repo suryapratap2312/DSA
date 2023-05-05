51695b3d659f11ee04e8481ae2aedac973f814e8
suryapratap2312 <87615712+suryapratap2312@users.noreply.github.com>
2023-05-05 19:28:46 +0530

    Create WordCount.py

---
+++ WordCount.py
@@ -0,0 +1,12 @@
+class Solution:
+    def prefixCount(self, words: List[str], pref: str) -> int:
+        result = 0
+        for word in words:
+            if word.startswith(pref):
+                result +=1
+        return result
+        
+
+#Second Method
+return sum([word.startswith(pref)  for word in words])
+        
